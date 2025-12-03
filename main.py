710d3945bdc6de176f626d397287bd6419d0b0de
distortedgoose <maph0002@brgym.dk>
2025-12-03 13:09:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #Go here to the URL shown beneath and navigate to the online  test tool under the headline section:
 # 'Accessing the Astro Pi Replay Tool online ' the online tool is called ' Astro Pi Replay Tool' 
-
+ 
 # https://projects.raspberrypi.org/en/projects/mission-space-lab-creator-guide/2
 # Import the Camera class from the picamera-zero module
 """
